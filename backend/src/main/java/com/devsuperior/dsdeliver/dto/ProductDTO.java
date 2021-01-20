d596f3607fddcd0e5fb8959f947cc3338074406a
Renan Soares <renansoaresffc@gmail.com>
2021-01-20 19:04:37 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -0,0 +1,79 @@
+package com.devsuperior.dsdeliver.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+// Objeto para carregar os dados do produto na camada de Web para cima
+public class ProductDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
+	private String description;
+	private String imageUri;
+	
+	// Construtor vazio
+	public ProductDTO() {	
+	}
+
+	// Construtor com parâmetros
+	public ProductDTO(Long id, String name, Double price, String description, String imageUri) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+	
+	public ProductDTO(Product entity) {
+		id = entity.getId();
+		name = entity.getName();
+		price = entity.getPrice();
+		description = entity.getDescription();
+		imageUri = entity.getImageUri();
+	}
+	
+	// Getters e Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+}
